a467831991abb24f502e584a3a6c27302164f1d6
Alan Federico Emir Gutiérrez <alanegutierrez@hotmail.com>
2025-11-25 09:46:39 -0300

    Revert "Gráficos"
    This reverts commit d400d61c300b5191f7e6b22fd225d1f718288c3f.

--- src/app/hooks/useUserMap.ts
+++
@@ -1,6 +0,0 @@
-export function useUserMap(users: any[]) {
-  return users.reduce((acc, user) => {
-    acc[user.id] = user;
-    return acc;
-  }, {} as Record<string, { id: string; name: string }>);
-}
